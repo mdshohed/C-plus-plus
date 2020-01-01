a11378aa8210b5763db8044cd8ef8f4d9cf08dac
Md Shohed <49224753+mdshohed@users.noreply.github.com>
2020-01-01 10:53:10 -0500

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -34,3 +34,25 @@ int main() {
     cout<<leter<<"\n";
     return 0;
 }
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch;
+    char s[100];
+    char sen[100];
+    scanf("%c",&ch);
+    scanf("%s",s);
+    scanf("%*[\n]%[^\n]",sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s\n",sen);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
