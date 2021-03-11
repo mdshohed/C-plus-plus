6c904112a03c71c809dd4a7de57e35d405ca3b40
Shohedul Islam <49224753+mdshohed@users.noreply.github.com>
2021-03-11 12:57:45 +0600

    Create C-Plus 1: Variable.cpp

---
+++ C-Plus 1: Variable.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n = 2200;
+
+    float f = 23.4344;
+
+    double d = 23424.4545254;
+
+    char ch = 'a';
+
+    char s[100] = "Hello World!";
+
+    bool b = 1;
+
+    string str = "Hello";
+
+    printf( "%d\n",n);
+    printf( "%f\n",f);
+    printf( "%llf\n",d);
+    printf( "%c\n",ch);
+    printf( "%s\n",s);
+    printf( "%d\n",b);
+
+    cout << str << endl;
+}
